79616a4ec03063c2591b354094c7fba7d39f63bb
casey <cassandra.d.mclellan@gmail.com>
2022-02-19 11:59:39 +0000

    CHORE: added auth0 sdk

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,11 +4,16 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain="dev-bmksv1z5.us.auth0.com"
+    clientId="cUWLjwpD97rzYtpxSHfqIqqTo46QfY5L"
+    redirectUri={window.location.origin}
+  >
     <App />
-  </React.StrictMode>,
+  </Auth0Provider>,
   document.getElementById("root")
 );
 
